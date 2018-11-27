a822077bb12523664352880892e09a0a3f85044e
Jack <doherty9@purdue.edu>
2018-11-27 18:41:51 -0500

    added fields and constraints for User domain class

--- grails-app/domain/roz/User.groovy
+++ grails-app/domain/roz/User.groovy
@@ -1,7 +1,23 @@
 package roz
 
 class User {
+    String name
+    String position
+    String classification
+    String major
+    String college
+    String subteam
+    String email
+    String slackUID
 
     static constraints = {
+        name blank: false
+        position blank: false, inList: ["General Member", "Director", "Officer", "Advisor"]
+        classification blank: false
+        major blank: false
+        college blank: false
+        subteam blank: false
+        email blank: false, email: true
+        slackUID blank: false
     }
 }
