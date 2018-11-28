7e4500354d78162113411386783a5e09b9eef13f
Jack <doherty9@purdue.edu>
2018-11-27 21:17:05 -0500

    added AttendanceRecord fields and added a UID field to Meeting

--- grails-app/domain/roz/AttendanceRecord.groovy
+++ grails-app/domain/roz/AttendanceRecord.groovy
@@ -1,6 +1,11 @@
 package roz
 
+import java.sql.Time
+
 class AttendanceRecord {
+    String attendeeUID
+    Time inTime
+    String meetingUID
 
     static constraints = {
     }
--- grails-app/domain/roz/Meeting.groovy
+++ grails-app/domain/roz/Meeting.groovy
@@ -3,7 +3,8 @@ package roz
 import java.sql.Time
 
 class Meeting {
-    String name
+    String meetingUID
+    String friendlyName
     Time startTime //does this have a start day? Is it just based of a time epoch?
     String meetingOwnerUID //who can grant exceptions if required?
     String[] allowedPeople //blank means everyone allowed?
