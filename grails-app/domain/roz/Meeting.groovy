a5b02d55154fbfe7b5627aedcc38ba83f1eceac2
Jack <doherty9@purdue.edu>
2018-11-27 21:23:28 -0500

    edited Meeting field

--- grails-app/domain/roz/Meeting.groovy
+++ grails-app/domain/roz/Meeting.groovy
@@ -7,7 +7,7 @@ class Meeting {
     String friendlyName
     Time startTime //does this have a start day? Is it just based of a time epoch?
     String meetingOwnerUID //who can grant exceptions if required?
-    String[] allowedPeople //blank means everyone allowed?
+    //String[] allowedPeople //blank means everyone allowed???????
     boolean isRequired
 
     static constraints = {
