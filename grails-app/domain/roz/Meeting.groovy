f28baad5921870e8e662d3d7924fb1b46a894096
Jack <doherty9@purdue.edu>
2018-11-27 20:49:43 -0500

    added fields to Meeting

--- grails-app/domain/roz/Meeting.groovy
+++ grails-app/domain/roz/Meeting.groovy
@@ -1,6 +1,13 @@
 package roz
 
+import java.sql.Time
+
 class Meeting {
+    String name
+    Time startTime //does this have a start day? Is it just based of a time epoch?
+    String meetingOwnerUID //who can grant exceptions if required?
+    String[] allowedPeople //blank means everyone allowed?
+    boolean isRequired
 
     static constraints = {
     }
