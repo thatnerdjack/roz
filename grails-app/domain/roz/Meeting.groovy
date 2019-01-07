5dc7caaa198ef11720211a2e4acbe1db266a6903
heatonk <heatonk@purdue.edu>
2019-01-07 16:04:52 -0500

    Schema changes

--- grails-app/domain/roz/Meeting.groovy
+++ grails-app/domain/roz/Meeting.groovy
@@ -5,11 +5,19 @@ import java.sql.Time
 class Meeting {
     String meetingUID
     String friendlyName
+    String type
     Time startTime //does this have a start day? Is it just based of a time epoch?
+    Time endTime
     String meetingOwnerUID //who can grant exceptions if required?
-    //String[] allowedPeople //blank means everyone allowed???????
-    boolean isRequired
+    String[] allowedPeople //blank means everyone allowed
 
     static constraints = {
+        meetingUID blank: false
+        friendlyName blank : false
+        type blank : false
+        startTime blank : false
+        endTime blank : false
+        meetingOwnerUID blank : false
+        allowedPeople blank : false
     }
 }
--- grails-app/domain/roz/User.groovy
+++ grails-app/domain/roz/User.groovy
@@ -9,15 +9,18 @@ class User {
     String subteam
     String email
     String slackUID
+    String inClub
 
     static constraints = {
         name blank: false
         position blank: false, inList: ["General Member", "Director", "Officer", "Advisor"]
+        year blank: false
         year blank: false
         major blank: false
         college blank: false
         subteam blank: false
         email blank: false, email: true
         slackUID blank: false
+        inClub blank : false
     }
 }
